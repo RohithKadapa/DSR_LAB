0167bda7e7964fce7ceef1831bbe4edb1ab19afa
RohithKadapa <57570340+RohithKadapa@users.noreply.github.com>
2019-11-09 23:26:38 +0530

    Add files via upload

---
+++ 4b/4b.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+library(gcookbook)
+cabbage_exp
+boxplot(Weight~Date,data=cabbage_exp,range=0,ylab="Weight ofCabbage",xlab="Dates of Cabbage",main="Rohith_Reddy_B1")
+print("Rohith_Reddy_B1")
